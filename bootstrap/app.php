cd51075e34138298e1c22b5f5c45ef2843e8e119
Silas Stoffel <silasstofel@gmail.com>
2021-12-23 19:23:25 -0300

    chore: routes setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,9 +23,9 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -110,6 +110,7 @@ $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
     require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/v1/v1.php';
 });
 
 return $app;
